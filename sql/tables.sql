29121abd123d9498c6372b39ac54652c1510b8a0
Hidaayat <hidaayat07@gmail.com>
2023-11-03 14:47:00 +0200

    changed sql query for table creation

--- sql/tables.sql
+++ sql/tables.sql
@@ -13,9 +13,10 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL
 )
 
 CREATE TABLE cart (
